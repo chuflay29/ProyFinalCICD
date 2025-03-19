cc7947974ecdf100da6913d459c7d3831f090c0a
VictorEstares <91997414+VictorEstares@users.noreply.github.com>
2025-03-19 15:24:47 -0400

    Update run.bat

--- _TestCaseM11/run.bat
+++ _TestCaseM11/run.bat
@@ -3,4 +3,4 @@ if [%1]==[] goto todo
 cucumber.bat %* --retry 2 -f pretty -f junit -out %CD%\ut
 goto :eof
 :todo
-cucumber.bat .\features\Cargar_fixtures\* .\features\Productos\* .\features\Cliente\* .\features\_Ventas\* --retry 2 -f pretty -f junit -out %CD%\ut
+cucumber.bat .\features\Productos\* .\features\Cliente\* .\features\Empleado\* --retry 2 -f pretty -f junit -out %CD%\ut
